481c9a1a4865cf6ccce497a3b9def24ece3cd156
Eugen Sawitzki <e.sawitzki@reply.de>
2024-02-05 09:50:15 +0100

    cleanup

--- src/main/kotlin/de/comsysto/addresssanitization/service/sanitization/SanitizationAssistant.kt
+++ src/main/kotlin/de/comsysto/addresssanitization/service/sanitization/SanitizationAssistant.kt
@@ -10,8 +10,7 @@ interface SanitizationAssistant {
         The following text includes a possibly incorrectly formatted german address. 
         Extract the street, zipCode, houseNumber and city from it and return it in the format:
         'street houseNumber, zipCode city'
-        If the zipCode is missing, try to retrieve it. 
-        If the zipCode is present in the input, use this one and do not try to retrieve it.
+        If some part of the address is missing, omit them in the output.
                 
         No explanation, just the formatted address.
         
